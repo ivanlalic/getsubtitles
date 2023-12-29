43094543976b3dc987c6ba8bfab0d20babc20f32
ivanlalic <ivanlalic@gmail.com>
2023-12-29 20:38:46 -0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wait for the DOM to be fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', () => {
-    // Get references to various HTML elements
     const urlInput = document.getElementById('url');
     const fileInput = document.getElementById('file');
     const getSubtitlesButton = document.getElementById('get-subtitles');
@@ -14,31 +12,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const processingMessage = document.getElementById('processing-message');
     const warningMessage = document.getElementById('warning-message');
 
-    // Function to hide the warning message
     const hideWarningMessage = () => {
         warningMessage.style.display = 'none';
     };
 
-    // Function to show the warning message
     const showWarningMessage = () => {
         warningMessage.style.display = 'block';
     };
 
-    // Function to start the processing indicators
     const startProcessing = () => {
         getSubtitlesButton.style.display = 'none';
         loadingSpinner.style.display = 'inline-block';
         processingMessage.style.display = 'inline';
     };
 
-    // Function to stop the processing indicators
     const stopProcessing = () => {
         getSubtitlesButton.style.display = 'none';
         loadingSpinner.style.display = 'none';
         processingMessage.style.display = 'none';
     };
 
-    // Function to display the transcription data in the UI
     const displayTranscription = (transcriptionData) => {
         fullTranscript.textContent = transcriptionData.full_transcript;
         downloadSRTButton.style.display = 'block';
@@ -48,26 +41,22 @@ document.addEventListener('DOMContentLoaded', () => {
         const srtContent = transcriptionData.subtitles.find(sub => sub.format === 'srt').subtitles;
         const vttContent = transcriptionData.subtitles.find(sub => sub.format === 'vtt').subtitles;
 
-        // Event listener for downloading SRT file
         downloadSRTButton.addEventListener('click', () => {
             downloadSubtitle(audioName + '.srt', srtContent, 'text/srt');
         });
 
-        // Event listener for downloading VTT file
         downloadVTTButton.addEventListener('click', () => {
             downloadSubtitle(audioName + '.vtt', vttContent, 'text/vtt');
         });
 
         transcriptContainer.style.display = 'block';
 
-        // Event listener to toggle transcript visibility
         toggleTranscriptButton.addEventListener('click', () => {
             const displayStyle = fullTranscript.style.display;
             fullTranscript.style.display = displayStyle === 'none' ? 'block' : 'none';
         });
     };
 
-    // Function to download a subtitle file
     const downloadSubtitle = (fileName, content, contentType) => {
         const subtitleLink = document.createElement('a');
         const subtitleBlob = new Blob([content], { type: contentType });
@@ -76,13 +65,11 @@ document.addEventListener('DOMContentLoaded', () => {
         subtitleLink.click();
     };
 
-    // Function to handle transcription data
     const handleTranscriptionData = (transcriptionData) => {
         displayTranscription(transcriptionData);
         stopProcessing(); // Call stopProcessing here to stop the processing indicators
     };
 
-    // Function to handle the "Get Subtitles" button click
     const handleGetSubtitlesClick = async () => {
         hideWarningMessage();
     
@@ -136,18 +123,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-    // Event listener for the "Get Subtitles" button click
     getSubtitlesButton.addEventListener('click', handleGetSubtitlesClick);
 
-    // WebSocket connection setup
     const socket = new WebSocket('wss://getsubtitlesserverv2.onrender.com');
 
-    // Event listener for WebSocket connection open
     socket.addEventListener('open', (event) => {
         console.log('WebSocket connection opened:', event);
     });
 
-    // Event listener for WebSocket messages
     socket.addEventListener('message', (event) => {
         const data = JSON.parse(event.data);
         console.log('WebSocket message received:', data);
@@ -157,7 +140,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Event listener for WebSocket connection close
     socket.addEventListener('close', (event) => {
         console.log('WebSocket connection closed:', event);
     });
